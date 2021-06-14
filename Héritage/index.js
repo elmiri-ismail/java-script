1a1f0709eca0e11ddf53454a2ac144d48b562145
Miri Ismail <ismailmiri@gmail.com>
2021-06-14 13:15:19 +0100

    Exemple 1 - POO héritage

---
+++ Héritage/index.js
@@ -0,0 +1,24 @@
+class vehicule {
+    nbporte;
+    nbroue;
+    
+        constructor ( nbporte , nbroue ){
+        
+            this.nbporte = nbporte
+            this.nbroue = nbroue
+        }
+    }
+    
+    class voiture extends vehicule {
+        marque;
+        constructor (marque){
+            super(4,4)
+            this.marque = marque
+           
+        }
+    }
+    
+    
+    
+    var voitures = new voiture ('AUDI')
+    console.log(voitures)
